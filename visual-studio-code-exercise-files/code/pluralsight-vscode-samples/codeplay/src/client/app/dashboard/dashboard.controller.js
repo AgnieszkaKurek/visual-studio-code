5a96c06270adde7e2005e5bbda9d4ae39d11df13
Agnieszka Kurek <agniechakurek@gmail.com>
2016-02-01 14:52:01 +0100

    Test commit

---
+++ visual-studio-code-exercise-files/code/pluralsight-vscode-samples/codeplay/src/client/app/dashboard/dashboard.controller.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+   
+
+  angular
+        .module('app.dashboard')
+        .controller('DashboardController', DashboardController);
+
+    DashboardController.$inject = ['$q', 'dataservice', 'logger'];
+    
+    function DashboardController($q, dataservice, logger) {
+        var vm = this;
+      
+        vm.news = {
+            title: 'codeplay',
+            description: 'Hot Towel  is a SPA template for Angular developers.'
+        };
+        vm.messageCount = 0;
+        vm.people = [];
+        vm.title = 'Dashboard';
+
+
+
+       
+
+        function getMessageCount() {
+            return dataservice.getMessageCount().then(function (data) {
+                vm.messageCount = data;
+                return vm.messageCount;
+            });
+        }
+
+        function getPeople() {
+            return dataservice.getPeople().then(function (data) {
+                vm.people = data;
+                return vm.people;
+            });
+        }
+    }
+})();
+
+
+
+
